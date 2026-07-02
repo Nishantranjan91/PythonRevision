e620021240a881cbd0a10530751db3109a1fb08e
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-02 09:34:54 +0530

    dunderMethod.py

---
+++ dunderMethod.py
@@ -0,0 +1,20 @@
+class Students:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+
+    def __str__(self):
+        return  f"{self.name} is your name and your marks are {self.marks}"    
+obj = Students("Nishant Ranjan",27)
+print(obj)        
+
+class Shopping:
+    def __init__(self,items):
+        self.items = items
+    def __len__(self):
+        return len(self.items)
+obj = Shopping(['apple',"milk","bread"])    
+obj2 = Shopping(["apple","banana"])    
+print(len(obj))
+print(len(obj2))
+        
