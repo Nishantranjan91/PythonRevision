ed810502fa1b125f8853419ab8a40d038790aca0
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-07 17:47:44 +0530

    for_loop_in_string.py

--- for_loop_in_string.py
+++ for_loop_in_string.py
@@ -10,6 +10,18 @@
 
 
 
+# s = "nishant ranjan"
+# for i in range(0,len(s),1):
+#     print(s[i])
+
+
+
+# s = "nishant ranjan"
+# for i in range(0,len(s),1):
+#     print(s[i])
+
+
+
 s = "nishant ranjan"
-for i in range(0,len(s),1):
+for i in range(len(s)):
     print(s[i])
