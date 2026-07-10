66ea4a58cd4c274993fa9e779f2d9f25fbea4e98
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-10 07:36:56 +0530

    mapFunction.py

--- mapFunction.py
+++ mapFunction.py
@@ -1,3 +1,13 @@
-a = [1,2,3,4,5]
-l = map(lambda x :x**2,a)
-print(list(l))
+# a = [1,2,3,4,5]
+# l = map(lambda x :x**2,a)
+# print(list(l))
+
+
+
+
+a = [1,2,3,4,5,6]
+l = []
+for i in a:
+    if i%2 == 0:
+        l.append(i)
+print(l)        
