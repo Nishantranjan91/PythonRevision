0dd397f7478e2a46303a8c2579153c66b0b44086
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-11 09:02:35 +0530

    exception.py

--- exception.py
+++ exception.py
@@ -10,14 +10,14 @@
 
 
 
-a = int(input("provide your numbers:-"))
-b = int(input("provide your numner:"))
+# a = int(input("provide your numbers:-"))
+# b = int(input("provide your numner:"))
 
-try:
-    print(a/b)
-except ZeroDivisionError as err:
-    print(f"sorry an error occured as {err}") 
-print(a+b)   
+# try:
+#     print(a/b)
+# except ZeroDivisionError as err:
+#     print(f"sorry an error occured as {err}") 
+# print(a+b)   
 
 
 
@@ -50,7 +50,7 @@ print(a+b)
 
 
 #  a = int(input("provide your numbers:-"))
-# # b = int(input("provide your numner:"))
+#  b = int(input("provide your numner:"))
 
 # try:
 #     print(a/b)
@@ -66,10 +66,10 @@ print(a+b)
 
 
 
-# try:
-#     age = int(input("Enter age:"))
-#     if age < 18:
-#         raise Exception("You must be 18+")
-#     print("access granted")
-# except Exception as e:
-#     print("Error:", e)
+try:
+    age = int(input("Enter age:"))
+    if age < 18:
+        raise Exception("You must be 18+")
+    print("access granted")
+except Exception as e:
+    print("Error:", e)
