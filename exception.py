978da0fed73239dc1f980ebee3ea8bc4f32e087c
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-11 08:55:18 +0530

    exception.py

---
+++ exception.py
@@ -0,0 +1,75 @@
+a = int(input("provide your numbers:-"))
+b = int(input("provide your numner:"))
+
+try:
+    print(a/b)
+except Exception as err:
+    print(f"sorry an error occured as {err}") 
+print(a+b)       
+
+
+
+
+# a = int(input("provide your numbers:-"))
+# b = int(input("provide your numner:"))
+
+# try:
+#     print(a/b)
+# except ZeroDivisionError as err:
+#     print(f"sorry an error occured as {err}") 
+# print(a+b)   
+
+
+
+
+# a = int(input("provide your numbers:-"))
+# b = int(input("provide your numner:"))
+
+# try:
+#     print(a/b)
+# except Exception as err:
+#     print(f"sorry an error occured as {err}") 
+# print(a+b)       
+
+
+
+
+#  a = int(input("provide your numbers:-"))
+# # b = int(input("provide your numner:"))
+
+# try:
+#     print(a/b)
+# except ZeroDivisionError as err:
+#     print(f"sorry an error occured as {err}") 
+# else:
+#     print("There was no errors")    
+# print(a+b)  
+
+
+
+
+
+#  a = int(input("provide your numbers:-"))
+# # b = int(input("provide your numner:"))
+
+# try:
+#     print(a/b)
+# except ZeroDivisionError as err:
+#     print(f"sorry an error occured as {err}") 
+# else:
+#     print("There was no errors")  
+# finally:
+#     print("I will execute no matter what !!")
+# print(a+b)
+
+
+
+
+
+# try:
+#     age = int(input("Enter age:"))
+#     if age < 18:
+#         raise Exception("You must be 18+")
+#     print("access granted")
+# except Exception as e:
+#     print("Error:", e)
---
+++ fileHandling.py
@@ -0,0 +1,15 @@
+# file = open("append.py")
+# print(file.read())
+
+
+
+
+
+# file = open("C:\Users\HP\Pictures\Screenshots\Screenshot 2026-06-26 172953.png")
+# print(file.read())
+
+
+
+
+with open("append.py",'r') as fs:
+    print(fs.read())
