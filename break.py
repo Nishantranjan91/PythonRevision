1dda55a8a44fd41e97411ad395b36531ad7887be
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-12 20:26:47 +0530

    break.py

--- break.py
+++ break.py
@@ -1,4 +1,18 @@
-for i in range(1,11):
+# for i in range(1,11):
+#     print(i)
+#     if i==5:
+#         break
+
+
+
+
+
+for i in range(10,20):
     print(i)
     if i==5:
         break
+
+
+
+
+
