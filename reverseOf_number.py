fe3c9b6c157812c08d8ad4fecb18a104ba9d6957
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-20 19:00:17 +0530

    reverseOf_number.py

--- reverseOf_number.py
+++ reverseOf_number.py
@@ -6,6 +6,15 @@
 
 
 
+# a = int(input("provide a number whose reverse is find you:"))
+
+# while a > 0:
+#     print(a % 10)
+#     a = a // 10
+
+
+
+
 a = int(input("provide a number whose reverse is find you:"))
 
 while a > 0:
