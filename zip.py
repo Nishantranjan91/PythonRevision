2d33eaf8d9b201b0804136a3d0833ddc23c0009b
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-23 21:00:26 +0530

    zip.py

--- zip.py
+++ zip.py
@@ -1,4 +1,13 @@
+# name = ["Nishant", "Santosh","Vivek"]
+# ages = [27,24,22]
+# comb = zip(name,ages)    
+# print ( dict(list(comb)))
+
+
+
+
+
 name = ["Nishant", "Santosh","Vivek"]
-ages = [27,24,22]
+ages = [27,24,22,33]
 comb = zip(name,ages)    
 print ( dict(list(comb)))
