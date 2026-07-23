d1f195c8c8849fffa62f9479664508714db2b46f
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-23 21:07:32 +0530

    method_overriding.py

--- method_overriding.py
+++ method_overriding.py
@@ -1,5 +1,21 @@
+# class Animal:
+#     name = "lion"
+#     def speak(self):
+#         print("hello I roar")
+# class Human(Animal):
+#     name = "Nishant Ranjan"
+#     def speak(self):
+#         print("hello I tweet")
+# obj = Human()
+# obj.speak()
+
+
+
+
+
+
 class Animal:
-    name = "lion"
+    name = "sher"
     def speak(self):
         print("hello I roar")
 class Human(Animal):
