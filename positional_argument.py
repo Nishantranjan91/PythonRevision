2d919aad3b848c8e2294b0869af678a2efdaf4ae
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-23 21:03:31 +0530

    positional_argument.py

--- positional_argument.py
+++ positional_argument.py
@@ -1,4 +1,14 @@
-def addition(a,b,c):
-    print(a+b+c)
+# def addition(a,b,c):
+#     print(a+b+c)
 
-addition(12,b=13,c=14)
+# addition(12,b=13,c=14)
+
+
+
+
+
+
+def multiplication(a,b,c):
+    print(a*b*c)
+
+multiplication(12,b=10,c=4)
