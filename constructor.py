91499909a4fb086c8e545e0016e3b017fbb967c5
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-24 11:21:11 +0530

    constructor.py

--- constructor.py
+++ constructor.py
@@ -1,9 +1,22 @@
-class Factory:
+# class Factory:
+#     def __init__(self,material,zips,pockets):
+#         self.material = material
+#         self.zips = zips
+#         self.pockets = pockets
+# reebok = Factory("Leather",3,3)
+# campus = Factory("nylon",2,2)
+# print(reebok.material)        
+
+
+
+
+
+
+class Menu:
     def __init__(self,material,zips,pockets):
         self.material = material
         self.zips = zips
         self.pockets = pockets
-reebok = Factory("Leather",3,3)
-campus = Factory("nylon",2,2)
-print(reebok.material)        
-
+reebok = Menu("Leather",3,3)
+campus = Menu("nylon",2,2)
+print(reebok.material) 
