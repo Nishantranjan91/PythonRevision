fa771c57a9c59b85df8139855128cd6e1bd29bce
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-25 16:39:09 +0530

    table_of_any_number.py

--- table_of_any_number.py
+++ table_of_any_number.py
@@ -1,4 +1,12 @@
+# n = int(input("tell me a number whose table you want:"))
+# for i in range(n,(n*10)+1,n):
+#     print(i)
+
+
+
+
+
+
 n = int(input("tell me a number whose table you want:"))
 for i in range(n,(n*10)+1,n):
     print(i)
-
