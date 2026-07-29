9e02ee557e93b6b907358ae03b38f3addf7ea89e
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-29 18:01:21 +0530

    sorting_list.py

--- sorting_list.py
+++ sorting_list.py
@@ -1,10 +1,25 @@
-a = [12,13,14,15,16,17,14,23,24,25,36,78,90]
+# a = [12,13,14,15,16,17,14,23,24,25,36,78,90]
 
-for i in range(len(a)):
-    if a[i] < a[i+1]:
+# for i in range(len(a)):
+#     if a[i] < a[i+1]:
+#         continue
+#     else:
+#         print("your list is not sorted.")
+#         break
+# else:
+#     print("your list is sorted")     
+
+
+
+
+
+b = [12,13,14,15,16,17,14,23,24,25,36,78,90,100,200]
+
+for i in range(len(b)):
+    if b[i] < b[i+1]:
         continue
     else:
         print("your list is not sorted.")
         break
 else:
-    print("your list is sorted")     
+    print("your list is sorted") 
