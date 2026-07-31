3250fa4361797dd7f136425a82d707df7275cadf
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-31 17:38:06 +0530

    map_function.py

--- map_function.py
+++ map_function.py
@@ -1,8 +1,8 @@
-a = [1,2,3,4]
-l = []
-for i in a :
-    l.append(i**2)
-print(l)    
+# a = [1,2,3,4]
+# l = []
+# for i in a :
+#     l.append(i**2)
+# print(l)    
 
 
 
@@ -15,8 +15,17 @@ print(l)
 
 
 
-def square(x):
-    return  x**3
-a = [1,2,3,4,5,6,7]
-m = map(square,a)
-print(list(m))
+# def square(x):
+#     return  x**3
+# a = [1,2,3,4,5,6,7]
+# m = map(square,a)
+# print(list(m))
+
+
+
+
+def square(y):
+    return  y**5
+b = [1, 5, 8, 10]
+n = map(square,b)
+print(list(n))
