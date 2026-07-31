c7b4b6cbb190d2d19776d674114245437ce2e84e
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-07-31 17:43:47 +0530

    def_multiplication_function.py

--- def_additionFunction.py
+++ def_additionFunction.py
@@ -1,10 +1,17 @@
-def addition(a,b):
-    return a+b
-print(addition(12,13))
+# def addition(a,b):
+#     return a+b
+# print(addition(12,13))
 
 
-# second method
-square = lambda a: print(a**a)
-square(12)
-add = lambda x,y : x+y
-print(add(12,12))
+# # second method
+# square = lambda a: print(a**a)
+# square(12)
+# add = lambda x,y : x+y
+# print(add(12,12))
+
+
+
+
+def multiplication(a,b):
+    return a*b
+print(multiplication(25,10))
