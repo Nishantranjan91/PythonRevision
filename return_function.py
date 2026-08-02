e230d4c689e9ae51d8197b9529127d053abbf22f
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-02 14:17:01 +0530

    return_function.py

--- return_function.py
+++ return_function.py
@@ -1,9 +1,24 @@
 # Function that returns the sum of two numbers
-def add(a, b):
-    return a + b
+# def add(a, b):
+#     return a + b
 
-# Call the function
-result = add(10, 20)
+# # Call the function
+# result = add(10, 20)
 
-# Print the returned value
-print("Sum =", result)
+# # Print the returned value
+# print("Sum =", result)
+
+
+# Function that checks if a number is even
+def is_even(number):
+    if number % 2 == 0:
+        return True
+    return False
+
+# Test
+num = 8
+
+if is_even(num):
+    print(num, "is even")
+else:
+    print(num, "is odd")
