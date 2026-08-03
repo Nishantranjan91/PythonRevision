0c092bbb61546b0b6eb5bc8f80907032f884b1e6
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-03 19:13:04 +0530

    attribute_method.py

--- attribute_method.py
+++ attribute_method.py
@@ -1,6 +1,14 @@
-class Factory:
-    a = 12 
+# class Factory:
+#     a = 12 
+#     def hello():
+#         print("how are you")
+# print(Factory.a)            
+# Factory.hello()
+
+
+class village:
+    a = 15 
     def hello():
         print("how are you")
-print(Factory.a)            
-Factory.hello()
+print(village.a)            
+village.hello()
