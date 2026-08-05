73127d4187f5e417586269e5ea62488bd0a6c230
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-05 14:10:43 +0530

    inheritance.py

--- inheritance.py
+++ inheritance.py
@@ -17,34 +17,62 @@
 
 
 
-class Vehicle:
-    def __init__(self, brand, year):
-        self.brand = brand
-        self.year = year
+# class Vehicle:
+#     def __init__(self, brand, year):
+#         self.brand = brand
+#         self.year = year
 
-    def info(self):
-        print(f"Brand: {self.brand}")
-        print(f"Year: {self.year}")
-
-
-class Car(Vehicle):
-    def __init__(self, brand, year, model, price):
-        super().__init__(brand, year)
-        self.model = model
-        self.price = price
-
-    def details(self):
-        self.info()   # Calling parent class method
-        print(f"Model: {self.model}")
-        print(f"Price: ₹{self.price}")
+#     def info(self):
+#         print(f"Brand: {self.brand}")
+#         print(f"Year: {self.year}")
 
 
-# Object of Parent Class
-obj1 = Vehicle("Toyota", 2022)
-obj1.info()
+# class Car(Vehicle):
+#     def __init__(self, brand, year, model, price):
+#         super().__init__(brand, year)
+#         self.model = model
+#         self.price = price
 
-print("--------------------")
+#     def details(self):
+#         self.info()   # Calling parent class method
+#         print(f"Model: {self.model}")
+#         print(f"Price: ₹{self.price}")
 
-# Object of Child Class
-obj2 = Car("Honda", 2024, "City", 1500000)
-obj2.details()
+
+# # Object of Parent Class
+# obj1 = Vehicle("Toyota", 2022)
+# obj1.info()
+
+# print("--------------------")
+
+# # Object of Child Class
+# obj2 = Car("Honda", 2024, "City", 1500000)
+# obj2.details()
+
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print(f"Name : {self.name}")
+        print(f"Age  : {self.age}")
+
+
+class Student(Person):
+    def __init__(self, name, age, roll, course):
+        super().__init__(name, age)
+        self.roll = roll
+        self.course = course
+
+    def student_info(self):
+        self.display()
+        print(f"Roll   : {self.roll}")
+        print(f"Course : {self.course}")
+
+
+student = Student("Nishant", 22, 101, "B.Tech")
+
+student.student_info()
