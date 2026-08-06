d46a9dd0c8c5748b49dcba6171ec119bcc6e1e02
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-06 17:42:41 +0530

    escapeSequencing.py

--- escapeSequencing.py
+++ escapeSequencing.py
@@ -1,9 +1,12 @@
-print(f"my name is nishant ranjan\nand my age is 27")
+# print(f"my name is nishant ranjan\nand my age is 27")
 
 
 
-print(f"my name is nishant ranjan\tand my age is 27")
+# print(f"my name is nishant ranjan\tand my age is 27")
 
 
 
-print(f"my name is nishant ranjan\band my age is 27")
+# print(f"my name is nishant ranjan\band my age is 27")
+
+
+print(r"my name is nishant ranjan\tand my age is 27")
