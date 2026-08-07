1b7815990921338865e7f4aafa3b5422eae1e8ac
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-07 15:39:02 +0530

    elifExample.py

---
+++ elifExample.py
@@ -0,0 +1,8 @@
+a = float(input("please provide first number:"))
+b = float(input("please provide second number:"))
+if a>b:
+    print(f"{a}is greater than {b}")
+elif b>a:
+    print(f"{b} is greater than {a}")
+else:
+    print(f"{a} and {b} both are equal")         
