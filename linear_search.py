851824735ecd850e301eaf466cc36ce9c9b4ead1
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-10 20:16:31 +0530

    linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,8 +1,22 @@
-a =[23,67,123,1,54,7,98,45,23,13,6,68]
-search = 6
+# a =[23,67,123,1,54,7,98,45,23,13,6,68]
+# search = 6
+# for i in range(len(a)):
+#     if a[i] == search:
+#         print(f"element found at index {i}")
+#         break
+# else:
+#     print("sorry no such element are exist")    
+
+
+
+
+a = [12, 34, 56, 78, 45, 23, 67, 89, 10]
+
+search = 45
+
 for i in range(len(a)):
     if a[i] == search:
-        print(f"element found at index {i}")
+        print(f"Element found at index {i}")
         break
 else:
-    print("sorry no such element are exist")    
+    print("Sorry, no such element exists")
