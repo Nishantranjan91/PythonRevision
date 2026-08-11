ad93edae08c04023ab67003d3142022bbbaed046
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-11 18:39:10 +0530

    For_loop_revision.py

---
+++ For_loop_revision.py
@@ -0,0 +1,15 @@
+# n = int(input("provide a number: "))
+# sum = 0
+# for i in range(1,n+1):
+#     sum = sum + i
+# print(f"your total sum is {sum}")    
+
+
+
+
+n = int(input("please give a number: "))
+fact = 1
+for i in range(n,0,-1):
+    fact = fact*i
+print(f"factorial of {n} is {fact}.")    
+
