c0d8ae0321f3173923a0cf63c17bcce33caf0b2c
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-12 17:53:38 +0530

    print10to0withWhileLoop.py

--- sum_of_digits_of_number.py
+++ sum_of_digits_of_number.py
@@ -1,10 +1,17 @@
-def sumOfDigits(n):
-    n = abs(n)
-    total = 0
-    while n > 0:
-        total += n % 10
-        n //= 10
-    return total
+# def sumOfDigits(n):
+#     n = abs(n)
+#     total = 0
+#     while n > 0:
+#         total += n % 10
+#         n //= 10
+#     return total
 
-n = int(input())
-print(sumOfDigits(n))
+# n = int(input())
+# print(sumOfDigits(n))
+
+
+
+
+n = int(input("provide a number : "))
+total = 0
+for digit in str(n)
