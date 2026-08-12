501e28719a96e91e98dc8ca66a7c5afb2ff4b579
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-12 09:28:29 +0530

    prime.py

--- prime.py
+++ prime.py
@@ -1,12 +1,25 @@
+# n = int(input("provide a number to find the no is prime or not "))
+# count = 0
+# for i in range(1,n+1,1):
+#     if n % i == 0:
+#         count = count + 1
+# if count == 2:
+#     print(f"{n} is a prime number")   
+# else:
+#     print(f"{n} is not a prime number")         
+
+    
+
+
+
 n = int(input("provide a number to find the no is prime or not "))
 count = 0
 for i in range(1,n+1,1):
     if n % i == 0:
         count = count + 1
-if count == 2:
+if count == 1:
+    print(f"{n} is a unity number")        
+elif count == 2:
     print(f"{n} is a prime number")   
 else:
-    print(f"{n} is not a prime number")         
-
-    
-
+    print(f"{n} is not a prime number")
