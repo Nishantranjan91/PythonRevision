1f537c55eef2f051d8cca84328393ef84665b6c5
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-12 09:23:50 +0530

    prime.py

---
+++ prime.py
@@ -0,0 +1,12 @@
+n = int(input("provide a number to find the no is prime or not "))
+count = 0
+for i in range(1,n+1,1):
+    if n % i == 0:
+        count = count + 1
+if count == 2:
+    print(f"{n} is a prime number")   
+else:
+    print(f"{n} is not a prime number")         
+
+    
+
