e0000041840c8b5cd88065450f4f8cd45edcf93c
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-15 18:45:23 +0530

    callingFunction.py

--- callingFunction.py
+++ callingFunction.py
@@ -4,18 +4,26 @@
 
 
 
-def Pallindrome(x):
-    rev = 0
-    copy = x
-    while x>0:
-        rev = (rev*10)+(x%10)
-        x = x//10
+# def Pallindrome(x):
+#     rev = 0
+#     copy = x
+#     while x>0:
+#         rev = (rev*10)+(x%10)
+#         x = x//10
 
-    if rev == copy:
-        return True
-    else:
-        return False
-print(Pallindrome(121))  
-print(Pallindrome(45654))
-print(Pallindrome(78987))    
-print(Pallindrome(125478))      
+#     if rev == copy:
+#         return True
+#     else:
+#         return False
+# print(Pallindrome(121))  
+# print(Pallindrome(45654))
+# print(Pallindrome(78987))    
+# print(Pallindrome(125478))      
+
+
+
+
+
+def addition(a,b):
+    print(a+b)
+addition(b=15,a=20)    
