62e8605be78ce2bb7beda53190a2d4cb1f97e2de
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-17 12:51:07 +0530

    deep_copy.py

--- deep_copy.py
+++ deep_copy.py
@@ -1,6 +1,20 @@
+# import copy
+# a = [10,20,30,40,50]
+# b = copy.deepcopy(a)
+# b[2] = 1000
+# print(a)
+# print(b)
+
+
+
+
+
 import copy
-a = [10,20,30,40,50]
-b = copy.deepcopy(a)
-b[2] = 1000
+a = [[10,20],[30,40]]
+b1 = a.copy()
+b2 = copy.deepcopy(a)
+b1 = [0][0] = 999
+b2 = [1][0] = 888
 print(a)
-print(b)
+print(b1)
+print(b2)
