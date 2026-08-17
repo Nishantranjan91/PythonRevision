8a2f465db1acf422fafa47a24888000d400f9d75
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-17 16:46:41 +0530

    traversingOfLoop.py

--- traversingOfLoop.py
+++ traversingOfLoop.py
@@ -1,3 +1,11 @@
-a = [10,20,30,40,50]
-for i in a:
-    print(i)
+# a = [10,20,30,40,50]
+# for i in a:
+#     print(i)
+
+
+
+
+#traversing method 2
+a = a = [10,20,30,40,50]
+for i in range(len(a)):
+    print(a[i])
