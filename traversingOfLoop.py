ee7b19900f502973154030e7d9d985dc103c97c3
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-17 18:23:31 +0530

    appendInPython.py

--- traversingOfLoop.py
+++ traversingOfLoop.py
@@ -6,6 +6,12 @@
 
 
 #traversing method 2
-a = a = [10,20,30,40,50]
-for i in range(len(a)):
-    print(a[i])
+# a = a = [10,20,30,40,50]
+# for i in range(len(a)):
+#     print(a[i])
+
+
+
+
+
+help(list)
