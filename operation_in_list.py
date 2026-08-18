d64b50b468cc13eb06746628c78fa984384ab46b
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-18 17:23:35 +0530

    operation_in_list.py

--- operation_in_list.py
+++ operation_in_list.py
@@ -8,8 +8,21 @@
 
 
 
-a = [22,32,42,52,62,72]
-sum = 0
-for i in a:
-    sum = sum+i
-print(f"average ot the total elements in this list is {sum/len(a)}")
+# a = [22,32,42,52,62,72]
+# sum = 0
+# for i in a:
+#     sum = sum+i
+# print(f"average ot the total elements in this list is {sum/len(a)}")
+
+
+
+
+
+a = [15,20,25,30,35,40,45,50]
+max = a[0]
+index = 0
+for i in range(len(a)):
+    if a[i]>max:
+        max =  a[i]
+        index = i
+print(f"your largest element is {max} and its index is {index}")        
