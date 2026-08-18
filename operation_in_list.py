d3c2c307ae227c9dde4ec483d795ace287499362
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-18 20:14:01 +0530

    operation_in_list.py

--- operation_in_list.py
+++ operation_in_list.py
@@ -18,11 +18,29 @@
 
 
 
-a = [15,20,25,30,35,40,45,50]
+# a = [15,20,25,30,35,40,45,50]
+# max = a[0]
+# index = 0
+# for i in range(len(a)):
+#     if a[i]>max:
+#         max =  a[i]
+#         index = i
+# print(f"your largest element is {max} and its index is {index}")        
+
+
+
+
+
+#Second greatest element
+a = [10,23,49,40,39,48,50]
 max = a[0]
+max2 = a[0]
 index = 0
+index2 = 0
 for i in range(len(a)):
     if a[i]>max:
-        max =  a[i]
+        max2 = max
+        max = a[i]
+        index2 = index
         index = i
-print(f"your largest element is {max} and its index is {index}")        
+print(f"max is {max} at {index} and max2 is {max2} at {index2}")        
