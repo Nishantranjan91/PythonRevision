c3fbb0c6ac0843e5cf8a986ca468c5054a39e18a
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-18 17:06:08 +0530

    operation_in_list.py

--- operation_in_list.py
+++ operation_in_list.py
@@ -1,5 +1,15 @@
+#find the sum of every elements of a list
+# a = [22,32,42,52,62,72]
+# sum = 0
+# for i in a:
+#     sum = sum+i
+# print(sum)    
+
+
+
+
 a = [22,32,42,52,62,72]
 sum = 0
 for i in a:
     sum = sum+i
-print(sum)    
+print(f"average ot the total elements in this list is {sum/len(a)}")
