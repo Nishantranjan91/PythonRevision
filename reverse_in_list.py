a6e905e937f276ab80e949044b2343eb2b017c15
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-19 14:39:14 +0530

    reverse_in_list.py

--- reverse_in_list.py
+++ reverse_in_list.py
@@ -5,8 +5,18 @@
 
 
 
+# a = [10,20,30,40,50]
+# b = []
+# for i in range(len(a)-1,-1,-1):
+#     b.append(a[i])
+# print(b)    
+
+
+
+
 a = [10,20,30,40,50]
-b = []
-for i in range(len(a)-1,-1,-1):
-    b.append(a[i])
-print(b)    
+b = len(a)-1
+for i in range(len(a)//2):
+    a[i],a[b] = a[b],a[i]
+    b= b-1
+print(a)    
