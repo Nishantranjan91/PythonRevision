69ae6750a2855a255546205304222390db776712
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-20 19:48:29 +0530

    bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,6 +1,17 @@
+# a = [12,34,98,76,49,30,490,23,21,99]
+# for j in range(len(a)-1):
+#     for i in range(len(a)-1):
+#         if a[i]>a[i+1]:
+#             a[i],a[i+1] = a[i+1],a[i]
+# print(a)            
+
+
+
+
+
 a = [12,34,98,76,49,30,490,23,21,99]
 for j in range(len(a)-1):
-    for i in range(len(a)-1):
+    for i in range(len(a)-1-j):
         if a[i]>a[i+1]:
             a[i],a[i+1] = a[i+1],a[i]
-print(a)            
+print(a)   
