93cd74106495553f6caeb8a8584ab931410a9d57
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-21 09:34:14 +0530

    tuple_traversing.py

--- tuple_traversing.py
+++ tuple_traversing.py
@@ -1,4 +1,12 @@
 #first way 
+# a = (12,13,14,15,16,39,33,55,88,98)
+# for i in a:
+#     print(i)
+
+
+
+
+
 a = (12,13,14,15,16,39,33,55,88,98)
-for i in a:
-    print(i)
+for i in range(len(a)):
+    print(a[i])
