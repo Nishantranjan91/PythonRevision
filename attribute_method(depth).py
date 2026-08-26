cc22585258e24900bdbf1cdd2186e25301e18695
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-26 20:59:29 +0530

    attribute_method(depth).py

--- attribute_method(depth).py
+++ attribute_method(depth).py
@@ -1,18 +1,44 @@
-class Animal:
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age 
+# class Animal:
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age 
 
         
+#     def info(self):
+#         print("this is a method")
+#     @classmethod
+#     def clmethod(cls):
+#         print(f"{cls.gender} is your gender")
+#     @staticmethod    
+#     def hello():
+#         print("hello I am static method")    
+# obj = Animal("Lion",12)        
+# obj.info()
+# obj.clmethod()
+# obj.hello()
+
+
+
+
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
     def info(self):
-        print("this is a method")
+        print(f"My name is {self.name} and my age is {self.age}")
+
     @classmethod
-    def clmethod(cls):
-        print(f"{cls.gender} is your gender")
-    @staticmethod    
+    def college(cls):
+        print("This student belongs to ABC College")
+
+    @staticmethod
     def hello():
-        print("hello I am static method")    
-obj = Animal("Lion",12)        
+        print("Hello I am a static method")
+
+
+obj = Student("Rahul", 20)
+
 obj.info()
-obj.clmethod()
+obj.college()
 obj.hello()
