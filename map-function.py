9e9fbece7f70b2ddf589133898fe83d883853b6b
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-27 19:28:37 +0530

    map-function.py

--- map-function.py
+++ map-function.py
@@ -1,5 +1,12 @@
+# a = [1,2,3,4,5]
+# l = []
+# for i in a:
+#     l.append(i**2)
+# print(l)    
+
+
+
+
 a = [1,2,3,4,5]
-l = []
-for i in a:
-    l.append(i**2)
-print(l)    
+l = map(lambda x :x**2,a)
+print(list(l))
