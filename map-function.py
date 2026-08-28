6b3fe8734a1d2a9a7115aa11b004f0f8b5b38e84
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-28 18:37:48 +0530

    map-function.py

--- map-function.py
+++ map-function.py
@@ -25,9 +25,19 @@
 
 
 #filter
+# a = [1,2,3,4,5,6,8,9,10]
+# l = []
+# for i in a:
+#     if i%2 ==0:
+#         l.append(i)
+# print(l)        
+
+
+
+
 a = [1,2,3,4,5,6,8,9,10]
 l = []
 for i in a:
-    if i%2 ==0:
+    if i%2 != 0:
         l.append(i)
 print(l)        
