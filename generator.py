1602bcd36dae53b8f79d41353509d3308cf809f9
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-30 18:54:15 +0530

    generator.py

--- generator.py
+++ generator.py
@@ -23,19 +23,19 @@
 
 
 
-def my_generator():
-    for i in range(5):
-        yield i
-gen = my_generator()
-print(next(gen))      
-print(next(gen))
-print(list(gen)) 
+# def my_generator():
+#     for i in range(5):
+#         yield i
+# gen = my_generator()
+# print(next(gen))      
+# print(next(gen))
+# print(list(gen)) 
 
 
 
 
 
-# generator comprehension
-# sequence = (x**2 for x in range(5))
-# print(next(sequence))
-# print(next(sequence))
+#generator comprehension
+sequence = (x**2 for x in range(5))
+print(next(sequence))
+print(next(sequence))
