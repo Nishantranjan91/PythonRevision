97b8596bca0f42a523dbb5afd2df7153556af124
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-08-31 16:19:12 +0530

    decorators.py

---
+++ decorators.py
@@ -0,0 +1,38 @@
+def say_hello():
+    print("hello")
+say_hello()    
+
+
+
+
+# def my_decorator(func):
+#     def wrapper():
+#         print("hello I will print before")
+#         func()  
+#         print("hello I will print after")
+#     return wrapper
+
+# @my_decorator
+# def say_hello():
+#     print("hello")
+# say_hello()
+
+
+
+
+
+
+# def decorate(func):
+#     def wrapper(a,b):
+#         print("your two numbers additions is: ")
+#         func(a,b)
+#         print("Thank you for using us")
+#     return wrapper    
+    
+
+
+
+
+# @decorate
+# def addition(a,b):
+#     print(a+b)
