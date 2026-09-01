3043dc014d1c415936ee02eb366efd309889bd18
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-01 12:35:59 +0530

    exception_handling.py

--- exception_handling.py
+++ exception_handling.py
@@ -1,9 +1,22 @@
+# a = int(input("please provide the first number:"))
+# b = int(input("please provide the second number:"))
+
+# try:
+#     print(a/b)
+
+# except Exception as err:
+#     print(f"sorry an error occured as {err}")    
+# print(a+b)
+
+
+
+
 a = int(input("please provide the first number:"))
 b = int(input("please provide the second number:"))
 
 try:
     print(a/b)
 
-except Exception as err:
+except ZeroDivisionError as err:
     print(f"sorry an error occured as {err}")    
 print(a+b)
