c490e2510b5b99bd3f43a018ad003f4dd082311e
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-04 14:41:49 +0530

    file_handling.py

---
+++ file_handling.py
@@ -0,0 +1,42 @@
+# Read a file -
+file = open("filter.py")
+print(file.read())
+
+
+
+
+# file = open(r"C:\Users\HP\Downloads\PostgreSQL(class-10).pdf.main.py")
+# print(file.read())
+
+
+
+
+
+#create a function
+#open("null.txt",'x')
+
+
+
+
+# #write a file
+# file = open("push.txt",'w')
+# file.write("hello this is a sample file that I have created")
+# #close the file
+# file.close()
+
+
+
+
+
+
+# #write a file
+# file = open("push.txt",'a')
+# file.write("hello this is a sample file that I have created")
+# #close the file
+# file.close()
+
+
+
+
+# with open("prime.py",'r') as fs:
+#     print(fs.read())
