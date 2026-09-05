874ca827b1f3d97f024dd141f25744c22fdce874
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-05 18:42:47 +0530

    attribute&method.py

--- attribute&method.py
+++ attribute&method.py
@@ -16,9 +16,19 @@
 
 
 
+# class Coaching:
+#     a = 16 # attribute
+#     def hello(): # method
+#         print("I am a student")
+# print(Coaching.a) 
+# Coaching.hello()
+
+
+
 class Coaching:
     a = 16 # attribute
     def hello(): # method
         print("I am a student")
 print(Coaching.a) 
 Coaching.hello()
+print("I am getting initialized")
