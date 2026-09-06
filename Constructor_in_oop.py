be08832b3006f92c3acd31de32b9f537ed2026db
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-06 21:15:02 +0530

    Constructor_in_oop.py

--- Constructor_in_oop.py
+++ Constructor_in_oop.py
@@ -1,4 +1,13 @@
-def Factory(material,zips,pockets):
-    pass
-Factory(material=22,zips=22,pockets=22)
-print(Factory)
+# def Factory(material,zips,pockets):
+#     pass
+# Factory(material=22,zips=22,pockets=22)
+# print(Factory)
+
+
+
+
+
+class Factory:
+    def __init__(self):
+        print("It is raining cats and dogs")
+Factory()        
