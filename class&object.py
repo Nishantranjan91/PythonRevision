127d6f2268f70cc3fddd558982f01e7d6b3f9359
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-06 18:59:32 +0530

    class&object.py

--- class&object.py
+++ class&object.py
@@ -1,6 +1,18 @@
+# class Business:
+#     a = "hello I am attribute"
+#     def hello():
+#         print("hello I am a method")
+# obj = Business()
+# print(obj.a)        
+
+
+
+
 class Business:
     a = "hello I am attribute"
     def hello():
         print("hello I am a method")
-obj = Business()
-print(obj.a)        
+obj = Business() # obj becomes an object who can access anything
+obj2 = Business()
+print(obj.a) 
+Business.hello()
