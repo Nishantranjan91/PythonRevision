baeb2550439233909e4d7daf573d31480285af17
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-09 20:54:20 +0530

    Class_Object.py

--- Class_Object.py
+++ Class_Object.py
@@ -1,15 +1,38 @@
 # Create a class Student with name and roll_no. Display the student details.
-class Student:
-    def __init__(self, name, roll_no):
-        self.name = name
-        self.roll_no = roll_no
+# class Student:
+#     def __init__(self, name, roll_no):
+#         self.name = name
+#         self.roll_no = roll_no
 
-    def display(self):
-        print("Name:", self.name)
-        print("Roll No:", self.roll_no)
+#     def display(self):
+#         print("Name:", self.name)
+#         print("Roll No:", self.roll_no)
+
+
+# # Creating object
+# s1 = Student("Rahul", 101)
+
+# s1.display()
+
+
+
+
+
+# Create a class Rectangle with length and breadth. Calculate and display the area.
+class Rectangle:
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+
+    def area(self):
+        print("Area =", self.length * self.breadth)
 
 
 # Creating object
-s1 = Student("Rahul", 101)
+r1 = Rectangle(10, 5)
 
-s1.display()
+r1.area()
+
+
+
+
