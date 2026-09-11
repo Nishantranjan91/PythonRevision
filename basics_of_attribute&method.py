1ef8266837d8ed7f1fc4e41820b364f079f5a0f3
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-11 17:52:49 +0530

    basics_of_attribute&method.py

--- basics_of_attribute&method.py
+++ basics_of_attribute&method.py
@@ -15,16 +15,35 @@
 
 
 
-class Car:
-    def __init__(self, brand, color):
-        self.brand = brand      # Attribute
-        self.color = color      # Attribute
+# class Car:
+#     def __init__(self, brand, color):
+#         self.brand = brand      # Attribute
+#         self.color = color      # Attribute
 
-    def drive(self):            # Method
-        print("The car is driving.")
+#     def drive(self):            # Method
+#         print("The car is driving.")
 
-car1 = Car("Toyota", "Red")
+# car1 = Car("Toyota", "Red")
 
-print(car1.brand)
-print(car1.color)
-car1.drive()
+# print(car1.brand)
+# print(car1.color)
+# car1.drive()
+
+
+
+
+class BankAccount:
+    def __init__(self, name, balance):
+        self.name = name         # Attribute
+        self.balance = balance   # Attribute
+
+    def deposit(self, amount):   # Method
+        self.balance += amount
+        print("New balance:", self.balance)
+
+account = BankAccount("Amit", 5000)
+
+print(account.name)
+print(account.balance)
+
+account.deposit(2000)
