7adaa5af7b3d2e0c76dbfe4f9b065aaa74fa22c7
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-13 18:57:04 +0530

    attribute_and_method.py

--- attribute_and_method.py
+++ attribute_and_method.py
@@ -72,28 +72,58 @@
 
 
 
-class Employee:
-    def __init__(self, name, employee_id, salary):
-        self.name = name
-        self.employee_id = employee_id
-        self.salary = salary
+# class Employee:
+#     def __init__(self, name, employee_id, salary):
+#         self.name = name
+#         self.employee_id = employee_id
+#         self.salary = salary
 
-    def display_details(self):
-        print("Name:", self.name)
-        print("Employee ID:", self.employee_id)
-        print("Salary:", self.salary)
+#     def display_details(self):
+#         print("Name:", self.name)
+#         print("Employee ID:", self.employee_id)
+#         print("Salary:", self.salary)
 
-    def increase_salary(self, percentage):
-        self.salary += self.salary * percentage / 100
+#     def increase_salary(self, percentage):
+#         self.salary += self.salary * percentage / 100
 
-    def annual_salary(self):
-        return self.salary * 12
+#     def annual_salary(self):
+#         return self.salary * 12
 
 
-emp = Employee("Amit", 102, 30000)
+# emp = Employee("Amit", 102, 30000)
 
-emp.display_details()
-emp.increase_salary(10)
+# emp.display_details()
+# emp.increase_salary(10)
 
-print("New Salary:", emp.salary)
-print("Annual Salary:", emp.annual_salary())
+# print("New Salary:", emp.salary)
+# print("Annual Salary:", emp.annual_salary())
+
+
+
+
+
+
+class MobilePhone:
+    def __init__(self, brand, model, battery):
+        self.brand = brand
+        self.model = model
+        self.battery = battery
+
+    def make_call(self, number):
+        print("Calling", number)
+
+    def charge(self, amount):
+        self.battery += amount
+
+        if self.battery > 100:
+            self.battery = 100
+
+    def show_battery(self):
+        print("Battery:", self.battery, "%")
+
+
+phone = MobilePhone("Samsung", "Galaxy S24", 50)
+
+phone.make_call("9876543210")
+phone.charge(30)
+phone.show_battery()
