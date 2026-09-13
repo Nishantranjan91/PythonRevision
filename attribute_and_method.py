66e201b77b1a1530044eb097869814b6cc6da2ac
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-13 18:50:49 +0530

    attribute_and_method.py

--- attribute_and_method.py
+++ attribute_and_method.py
@@ -1,26 +1,48 @@
-class Student:
-    def __init__(self, name, roll_no, marks):
-        self.name = name          # Attribute
-        self.roll_no = roll_no    # Attribute
-        self.marks = marks        # Attribute
+# class Student:
+#     def __init__(self, name, roll_no, marks):
+#         self.name = name          # Attribute
+#         self.roll_no = roll_no    # Attribute
+#         self.marks = marks        # Attribute
 
-    def display_details(self):    # Method
-        print("Name:", self.name)
-        print("Roll No:", self.roll_no)
-        print("Marks:", self.marks)
+#     def display_details(self):    # Method
+#         print("Name:", self.name)
+#         print("Roll No:", self.roll_no)
+#         print("Marks:", self.marks)
 
-    def calculate_grade(self):    # Method
-        if self.marks >= 90:
-            return "A"
-        elif self.marks >= 75:
-            return "B"
-        elif self.marks >= 60:
-            return "C"
+#     def calculate_grade(self):    # Method
+#         if self.marks >= 90:
+#             return "A"
+#         elif self.marks >= 75:
+#             return "B"
+#         elif self.marks >= 60:
+#             return "C"
+#         else:
+#             return "D"
+
+
+# s1 = Student("Rahul", 101, 82)
+
+# s1.display_details()
+# print("Grade:", s1.calculate_grade())
+
+
+
+
+
+
+class BankAccount:
+    def __init__(self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
         else:
-            return "D"
+            print("Insufficient balance")
 
-
-s1 = Student("Rahul", 101, 82)
-
-s1.display_details()
-print("Grade:", s1.calculate_grade())
+    def show_balance(self):
+        print("Current Balance:", self.balance)
