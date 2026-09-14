44ec83fc4cc34ca62a05a2bb2531e0d6cb6e08a6
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-14 23:16:42 +0530

    instance.py

--- instance.py
+++ instance.py
@@ -1,15 +1,37 @@
-    class Student:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+#     class Student:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def display(self):       # Instance method
-        print("Name:", self.name)
-        print("Age:", self.age)
+#     def display(self):       # Instance method
+#         print("Name:", self.name)
+#         print("Age:", self.age)
 
 
-# Creating an object (instance)
-s1 = Student("Rahul", 20)
+# # Creating an object (instance)
+# s1 = Student("Rahul", 20)
 
-# Calling instance method
-s1.display()
+# # Calling instance method
+# s1.display()
+
+
+
+
+
+
+class Calculator:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def add(self):           # Instance method
+        return self.a + self.b
+
+    def multiply(self):      # Instance method
+        return self.a * self.b
+
+
+c1 = Calculator(10, 5)
+
+print("Addition:", c1.add())
+print("Multiplication:", c1.multiply())
