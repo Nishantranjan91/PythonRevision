1d546849f450ea2fe55e2a4532f7d5a2831f3f1c
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-18 19:23:40 +0530

    Inheritance_.py

--- Inheritance_.py
+++ Inheritance_.py
@@ -1,27 +1,37 @@
-class Animal: # parent class
-    def __init__(self,name,age):
-        self.name = name
-        self.age = age
+# class Animal: # parent class
+#     def __init__(self,name,age):
+#         self.name = name
+#         self.age = age
 
-    def info(self):
-        print(f"your name is {self.name} and your age is {self.age}")    
-
-
+#     def info(self):
+#         print(f"your name is {self.name} and your age is {self.age}")    
 
 
 
-class human(Animal): # child class
-    def __init__(self, name, age, number, group):
-        super().__init__(name, age)
-        self.number = number
-        self.group = group
+
+
+# class human(Animal): # child class
+#     def __init__(self, name, age, number, group):
+#         super().__init__(name, age)
+#         self.number = number
+#         self.group = group
 
 
 
-obj = Animal("Lion",12)
-obj2 = human("Nishant Ranjan",27,1234, "B+")
-obj2.info()
-class Robots(human):
-    def __init__(self, name, age, number, group):
-        super().__init__(name, age, number, group)
-        self.imei = self.imei
+# obj = Animal("Lion",12)
+# obj2 = human("Nishant Ranjan",27,1234, "B+")
+# obj2.info()
+# class Robots(human):
+#     def __init__(self, name, age, number, group):
+#         super().__init__(name, age, number, group)
+#         self.imei = self.imei
+
+
+
+# multiple inheritance
+class Animal:
+    name = "Lion"
+class Human:
+    name = "Nishant"
+class Robots(Human,Animal):
+    pass        
