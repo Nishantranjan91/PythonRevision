394e10ba6c41408cadca434215b5457c4a2a244b
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-23 23:17:16 +0530

    polymorphism.py

--- polymorphism_.py
+++ polymorphism_.py
@@ -1,15 +1,44 @@
+# #Different classes can have a method with the same name but different behavior
+# class Dog:
+#     def sound(self):
+#         print("Dog says: Woof Woof")
+
+
+# class Cat:
+#     def sound(self):
+#         print("Cat says: Meow")
+
+
+# dog = Dog()
+# cat = Cat()
+
+# dog.sound()
+# cat.sound()
+
+
+
+
+# The same function can work with different types of objects.
 class Dog:
     def sound(self):
-        print("Dog says: Woof Woof")
+        return "Woof"
 
 
 class Cat:
     def sound(self):
-        print("Cat says: Meow")
+        return "Meow"
+
+
+def make_sound(animal):
+    print(animal.sound())
 
 
 dog = Dog()
 cat = Cat()
 
-dog.sound()
-cat.sound()
+make_sound(dog)
+make_sound(cat)
+
+
+
+
