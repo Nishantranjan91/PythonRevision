6885f53e80fc8883db20b713a9a2f749c6ef1a55
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-23 23:14:39 +0530

    polymorphism.py

---
+++ polymorphism_.py
@@ -0,0 +1,15 @@
+class Dog:
+    def sound(self):
+        print("Dog says: Woof Woof")
+
+
+class Cat:
+    def sound(self):
+        print("Cat says: Meow")
+
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
