6ac81c8bbe0c55efd111a1f6e1309d43b35cd48e
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-24 20:37:28 +0530

    abstraction_.py

--- abstraction_.py
+++ abstraction_.py
@@ -1,19 +1,50 @@
 #Basic Abstraction using ABC
 # from abc import ABC, abstractmethod
+# from abc import ABC, abstractmethod
+
+# class Animal(ABC):
+
+#     @abstractmethod
+#     def sound(self):
+#         pass
+
+
+# class Dog(Animal):
+
+#     def sound(self):
+#         print("Dog barks")
+
+
+# dog = Dog()
+# dog.sound()
+
+
+
+
+# Abstraction with Car
 from abc import ABC, abstractmethod
 
-class Animal(ABC):
+class Car(ABC):
 
     @abstractmethod
-    def sound(self):
+    def start(self):
         pass
 
 
-class Dog(Animal):
+class BMW(Car):
 
-    def sound(self):
-        print("Dog barks")
+    def start(self):
+        print("BMW starts with a button")
 
 
-dog = Dog()
-dog.sound()
+class Tesla(Car):
+
+    def start(self):
+        print("Tesla starts automatically")
+
+
+car1 = BMW()
+car1.start()
+
+car2 = Tesla()
+car2.start()
