41ce4d3e2f05af4449c7a88b688154909de190af
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-25 21:57:20 +0530

    basic_oop.py

---
+++ basic_oop.py
@@ -0,0 +1,11 @@
+# Concept: A class is a blueprint, and an object is an instance of that class.
+class Student:
+    def display(self):
+        print("Hello, I am a student")
+
+
+# Creating object
+s1 = Student()
+
+# Calling method
+s1.display()
