46deca7b2c12f3f2f0d4d2a46fae8959a2c6a25c
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-25 22:02:57 +0530

    basic_oop.py

--- basic_oop.py
+++ basic_oop.py
@@ -61,3 +61,23 @@ dog = Dog()
 
 dog.eat()
 dog.bark()
+
+
+
+
+# Concept: Polymorphism means "many forms" — the same method/interface can have different behavior.
+class Dog:
+    def sound(self):
+        print("Dog says: Woof")
+
+
+class Cat:
+    def sound(self):
+        print("Cat says: Meow")
+
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
