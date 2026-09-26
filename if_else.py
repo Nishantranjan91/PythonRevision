9f4b9a413078aee1e558315cf805747d3c8f6088
Nishant Ranjan <nishantranjj91555@gmail.com>
2026-09-27 01:14:11 +0530

    if_else.py

--- if_else.py
+++ if_else.py
@@ -3,3 +3,8 @@ if age >=18:
     print("you can vote")
 else:
     print("you can not vote")    
+
+
+
+# ternary operations:
+print("yes you can vote") if age >=18 else print("sorry you can not vote")    
